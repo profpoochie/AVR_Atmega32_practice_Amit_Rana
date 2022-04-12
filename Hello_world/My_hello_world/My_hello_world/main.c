55dd8045ddfa6c1b6366fb108cb7be9c9b9099e0
Marvin Borhillo <homepoochie@gmail.com>
2022-04-13 05:59:25 +0800

    Create hello_world with LEDs

---
+++ Hello_world/My_hello_world/My_hello_world/main.c
@@ -0,0 +1,29 @@
+/*
+ * My_hello_world.c
+ *
+ * Created: 04/13/2022 5:50:12 am
+ * Author : poochie
+ */ 
+
+#include <avr/io.h>
+#define F_CPU 16000000L		// Clock at 16MHz
+#include <util/delay.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+	
+	DDRB = 0xff;	// all the values is 1111 1111 all pins are output
+	
+	
+    while (1) 
+    {
+		// this code will repeat infinitely
+		PORTB = 0xff;
+		_delay_ms(1000);
+		PORTB = 0x00;
+		_delay_ms(1000);
+    }
+}
+
